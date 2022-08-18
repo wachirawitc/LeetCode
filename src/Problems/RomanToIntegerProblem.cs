85cb27de2ecbf5d4704f4e348b09962449fe366b
wachirawit <wachirawit.chankaew@gmail.com>
2022-08-18 13:45:25 +0700

    Resolve roman to integer
    https://leetcode.com/problems/roman-to-integer/

---
+++ src/Problems/RomanToIntegerProblem.cs
@@ -0,0 +1,79 @@
+﻿namespace Problems;
+
+public class RomanToIntegerProblem
+{
+    public int RomanToInt(string s)
+    {
+        var romans = s.ToCharArray();
+
+        var integers = new List<int>();
+        for (var index = 0; index < romans.Length; index++)
+        {
+            var current = romans.ElementAt(index);
+            var next = romans.ElementAtOrDefault(index + 1);
+
+            switch (current)
+            {
+                case 'I' when 'V'.Equals(next):
+                    integers.Add(4);
+                    index++;
+                    break;
+
+                case 'I' when 'X'.Equals(next):
+                    integers.Add(9);
+                    index++;
+                    break;
+
+                case 'I':
+                    integers.Add(1);
+                    break;
+
+                case 'V':
+                    integers.Add(5);
+                    break;
+
+                case 'X' when 'L'.Equals(next):
+                    integers.Add(40);
+                    index++;
+                    break;
+
+                case 'X' when 'C'.Equals(next):
+                    integers.Add(90);
+                    index++;
+                    break;
+
+                case 'X':
+                    integers.Add(10);
+                    break;
+
+                case 'L':
+                    integers.Add(50);
+                    break;
+
+                case 'C' when 'D'.Equals(next):
+                    integers.Add(400);
+                    index++;
+                    break;
+
+                case 'C' when 'M'.Equals(next):
+                    integers.Add(900);
+                    index++;
+                    break;
+
+                case 'C':
+                    integers.Add(100);
+                    break;
+
+                case 'D':
+                    integers.Add(500);
+                    break;
+
+                case 'M':
+                    integers.Add(1000);
+                    break;
+            }
+        }
+
+        return integers.Sum();
+    }
+}
---
+++ test/Problems.Test/RomanToIntegerProblemTest.cs
@@ -0,0 +1,56 @@
+﻿namespace Problems.Test;
+
+public class RomanToIntegerProblemTest
+{
+    [Fact]
+    public void Test1()
+    {
+        var problem = new RomanToIntegerProblem();
+
+        const string s = "III";
+
+        const int expected = 3;
+        var actual = problem.RomanToInt(s);
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        var problem = new RomanToIntegerProblem();
+
+        const string s = "LVIII";
+
+        const int expected = 58;
+        var actual = problem.RomanToInt(s);
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void Test3()
+    {
+        var problem = new RomanToIntegerProblem();
+
+        const string s = "MCMXCIV";
+
+        const int expected = 1994;
+        var actual = problem.RomanToInt(s);
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void Test4()
+    {
+        var problem = new RomanToIntegerProblem();
+
+        const string s = "DCXXI";
+
+        const int expected = 621;
+        var actual = problem.RomanToInt(s);
+
+        Assert.Equal(expected, actual);
+    }
+}
