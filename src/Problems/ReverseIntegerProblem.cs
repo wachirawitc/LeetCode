ecc3e7154132a5c0ec5cb08709ecd5516972ce4a
wachirawit <wachirawit.chankaew@gmail.com>
2022-08-18 17:58:15 +0700

    Reverse Integer
    https://leetcode.com/problems/reverse-integer/

---
+++ src/Problems/ReverseIntegerProblem.cs
@@ -0,0 +1,23 @@
+﻿namespace Problems;
+
+public class ReverseIntegerProblem
+{
+    public int Reverse(int x)
+    {
+        if (x is int.MinValue or int.MaxValue)
+        {
+            return 0;
+        }
+
+        var chars = Math.Abs(x).ToString().ToCharArray();
+        Array.Reverse(chars);
+
+        var text = new string(chars);
+        if (int.TryParse(text, out var answer) == false)
+        {
+            return 0;
+        }
+
+        return x < 0 ? answer * -1 : answer;
+    }
+}
---
+++ test/Problems.Test/ReverseIntegerProblemTest.cs
@@ -0,0 +1,54 @@
+﻿namespace Problems.Test;
+
+public class ReverseIntegerProblemTest
+{
+    [Fact]
+    public void Test1()
+    {
+        var reverseIntegerProblem = new ReverseIntegerProblem();
+        var actual = reverseIntegerProblem.Reverse(123);
+        const int expected = 321;
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        var reverseIntegerProblem = new ReverseIntegerProblem();
+        var actual = reverseIntegerProblem.Reverse(-123);
+        const int expected = -321;
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void Test3()
+    {
+        var reverseIntegerProblem = new ReverseIntegerProblem();
+        var actual = reverseIntegerProblem.Reverse(120);
+        const int expected = 21;
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void Test4()
+    {
+        var reverseIntegerProblem = new ReverseIntegerProblem();
+        var actual = reverseIntegerProblem.Reverse(1534236469);
+        const int expected = 0;
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void Test5()
+    {
+        var reverseIntegerProblem = new ReverseIntegerProblem();
+        var actual = reverseIntegerProblem.Reverse(-2147483648);
+        const int expected = 0;
+
+        Assert.Equal(expected, actual);
+    }
+}
