836ef5f3eea56c7e7fbb5819185a895f2b2a2441
wachirawit <wachirawit.chankaew@gmail.com>
2022-08-18 17:34:54 +0700

    Resolve Pow(x, n)
    https://leetcode.com/problems/powx-n

---
+++ src/Problems/PowProblem.cs
@@ -0,0 +1,11 @@
+﻿namespace Problems;
+
+public class PowProblem
+{
+    public double MyPow(double x, int n)
+    {
+        var row = Math.Pow(x, n);
+        var ans = row.ToString("#0.00000");
+        return double.Parse(ans);
+    }
+}
---
+++ test/Problems.Test/PowProblemTest.cs
@@ -0,0 +1,44 @@
+﻿namespace Problems.Test;
+
+public class PowProblemTest
+{
+    [Fact]
+    public void Test1()
+    {
+        var powProblem = new PowProblem();
+        var actual = powProblem.MyPow(2.00000, 10);
+        const double expected = 1024.00000;
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        var powProblem = new PowProblem();
+        var actual = powProblem.MyPow(2.10000, 3);
+        const double expected = 9.26100;
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void Test3()
+    {
+        var powProblem = new PowProblem();
+        var actual = powProblem.MyPow(2.00000, -2);
+        const double expected = 0.25000;
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void Test4()
+    {
+        var powProblem = new PowProblem();
+        var actual = powProblem.MyPow(8.88023, 3);
+        const double expected = 700.28148;
+
+        Assert.Equal(expected, actual);
+    }
+}
