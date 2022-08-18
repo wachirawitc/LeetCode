f965fe07a8dfe17d31df7ea3db1107b69a83c5e6
wachirawit <wachirawit.chankaew@gmail.com>
2022-08-18 14:37:54 +0700

    Resolve Merge Two Sorted Lists
    https://leetcode.com/problems/merge-two-sorted-lists/

---
+++ src/Problems/MergeTwoSortedListsProblem.cs
@@ -0,0 +1,62 @@
+﻿namespace Problems;
+
+public class MergeTwoSortedListsProblem
+{
+    public ListNode MergeTwoLists(ListNode list1, ListNode list2)
+    {
+        var numbers = new List<int>();
+        numbers.AddRange(ToNumbers(list1));
+        numbers.AddRange(ToNumbers(list2));
+
+        if (numbers.Any() == false)
+        {
+            return null;
+        }
+
+        numbers = numbers.OrderBy(value => value).ToList();
+
+        var node = new ListNode(numbers[0]);
+        numbers.RemoveAt(0);
+
+        CreateNode(ref node, numbers);
+
+        return node;
+    }
+
+    private static void CreateNode(ref ListNode previousNode, IList<int> numbers)
+    {
+        if (numbers.Any())
+        {
+            var nodeNext = new ListNode(numbers[0]);
+            previousNode.next = nodeNext;
+
+            numbers.RemoveAt(0);
+            CreateNode(ref nodeNext, numbers);
+        }
+    }
+
+    private static IEnumerable<int> ToNumbers(ListNode node)
+    {
+        var numbers = new List<int>();
+        while (node != null)
+        {
+            numbers.Add(node.val);
+            node = node.next;
+        }
+
+        return numbers;
+    }
+
+    public class ListNode
+    {
+        public ListNode next;
+
+        public int val;
+
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
---
+++ test/Problems.Test/MergeTwoSortedListsProblemTest.cs
@@ -0,0 +1,32 @@
+﻿using static Problems.MergeTwoSortedListsProblem;
+
+namespace Problems.Test;
+
+public class MergeTwoSortedListsProblemTest
+{
+    [Fact]
+    public void Test1()
+    {
+        var line1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+        var line2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+
+        var expected = new ListNode(1, new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(4))))));
+
+        var problem = new MergeTwoSortedListsProblem();
+        var actual = problem.MergeTwoLists(line1, line2);
+
+        Assert.Equal(ToArray(expected), ToArray(actual));
+    }
+
+    private static int[] ToArray(ListNode? node)
+    {
+        var items = new List<int>();
+        while (node != null)
+        {
+            items.Add(node.val);
+            node = node.next;
+        }
+
+        return items.ToArray();
+    }
+}
