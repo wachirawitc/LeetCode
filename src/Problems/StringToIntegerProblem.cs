2fadbeb6f52d7d48ce687e7c5e44b8a40d8271bf
wachirawit <wachirawit.chankaew@gmail.com>
2022-08-19 09:40:10 +0700

    String to Integer (atoi)
    https://leetcode.com/problems/string-to-integer-atoi/

---
+++ src/Problems/StringToIntegerProblem.cs
@@ -0,0 +1,60 @@
+﻿using System.Text;
+
+namespace Problems;
+
+/// <summary>
+/// String to Integer (atoi)
+/// https://leetcode.com/problems/string-to-integer-atoi/
+/// </summary>
+public class StringToIntegerProblem
+{
+    public int MyAtoi(string s)
+    {
+        if (string.IsNullOrWhiteSpace(s))
+        {
+            return 0;
+        }
+
+        s = s.Trim();
+
+        var isNegative = false;
+
+        var stack = new StringBuilder();
+        for (var index = 0; index < s.Length; index++)
+        {
+            var current = s[index];
+            if (index == 0 && '-'.Equals(current))
+            {
+                isNegative = true;
+            }
+            else if (index == 0 && '+'.Equals(current))
+            {
+                isNegative = false;
+            }
+            else if (char.IsDigit(current))
+            {
+                stack.Append(current);
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        var text = stack.ToString();
+        if (string.IsNullOrWhiteSpace(text))
+        {
+            return 0;
+        }
+
+        if (int.TryParse(text, out var answer))
+        {
+            return isNegative ? answer * -1 : answer;
+        }
+
+        const int minimumRange = -2147483648;
+        const int maximumRange = 2147483647;
+
+        return isNegative ? minimumRange : maximumRange;
+    }
+}
---
+++ test/Problems.Test/StringToIntegerProblemTest.cs
@@ -0,0 +1,104 @@
+﻿namespace Problems.Test;
+
+public class StringToIntegerProblemTest
+{
+    [Fact]
+    public void Test1()
+    {
+        var stringToIntegerProblem = new StringToIntegerProblem();
+        var actual = stringToIntegerProblem.MyAtoi("42");
+        const int expected = 42;
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void Test10()
+    {
+        var stringToIntegerProblem = new StringToIntegerProblem();
+        var actual = stringToIntegerProblem.MyAtoi("  -0012a42");
+        const int expected = -12;
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        var stringToIntegerProblem = new StringToIntegerProblem();
+        var actual = stringToIntegerProblem.MyAtoi("   -42");
+        const int expected = -42;
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void Test3()
+    {
+        var stringToIntegerProblem = new StringToIntegerProblem();
+        var actual = stringToIntegerProblem.MyAtoi("4193 with words");
+        const int expected = 4193;
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void Test4()
+    {
+        var stringToIntegerProblem = new StringToIntegerProblem();
+        var actual = stringToIntegerProblem.MyAtoi("words and 987");
+        const int expected = 0;
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void Test5()
+    {
+        var stringToIntegerProblem = new StringToIntegerProblem();
+        var actual = stringToIntegerProblem.MyAtoi("-91283472332");
+        const int expected = -2147483648;
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void Test6()
+    {
+        var stringToIntegerProblem = new StringToIntegerProblem();
+        var actual = stringToIntegerProblem.MyAtoi("3.14159");
+        const int expected = 3;
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void Test7()
+    {
+        var stringToIntegerProblem = new StringToIntegerProblem();
+        var actual = stringToIntegerProblem.MyAtoi(".1");
+        const int expected = 0;
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void Test8()
+    {
+        var stringToIntegerProblem = new StringToIntegerProblem();
+        var actual = stringToIntegerProblem.MyAtoi("-");
+        const int expected = 0;
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void Test9()
+    {
+        var stringToIntegerProblem = new StringToIntegerProblem();
+        var actual = stringToIntegerProblem.MyAtoi("+1");
+        const int expected = 1;
+
+        Assert.Equal(expected, actual);
+    }
+}
