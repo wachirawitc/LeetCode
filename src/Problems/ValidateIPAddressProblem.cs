cf50501659375f05c26028e9819295e0a3995100
wachirawit <wachirawit.chankaew@gmail.com>
2022-08-24 11:32:19 +0700

    Update validate ip

---
+++ src/Problems/ValidateIPAddressProblem.cs
@@ -0,0 +1,68 @@
+﻿namespace Problems;
+
+/// <summary>
+/// Validate IP Address
+/// https://leetcode.com/problems/validate-ip-address/
+/// </summary>
+public class ValidateIPAddressProblem
+{
+    public string ValidIPAddress(string queryIP)
+    {
+        const string neither = "Neither";
+
+        var v4 = queryIP.Split(".");
+        if (v4.Length == 4)
+        {
+            return IsV4(v4) ? "IPv4" : neither;
+        }
+
+        var v6 = queryIP.Split(":");
+        if (v6.Length == 8)
+        {
+            return IsV6(v6) ? "IPv6" : neither;
+        }
+
+        return neither;
+    }
+
+    private static bool IsV4(IEnumerable<string> classes)
+    {
+        foreach (var text in classes)
+        {
+            if (int.TryParse(text, out var answer) == false)
+            {
+                return false;
+            }
+
+            if (text[0] == '0')
+            {
+                return false;
+            }
+
+            if (answer is < 0 or > 255)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static bool IsV6(IEnumerable<string> classes)
+    {
+        foreach (var text in classes)
+        {
+            if (text.Length > 4)
+            {
+                return false;
+            }
+
+            if (int.TryParse(text, System.Globalization.NumberStyles.HexNumber, null, out int x) == false)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ test/Problems.Test/ValidateIPAddressProblemTest.cs
@@ -0,0 +1,54 @@
+﻿namespace Problems.Test;
+
+public class ValidateIPAddressProblemTest
+{
+    [Fact]
+    public void Test1()
+    {
+        var problem = new ValidateIPAddressProblem();
+        var actual = problem.ValidIPAddress("172.16.254.1");
+        const string expected = "IPv4";
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        var problem = new ValidateIPAddressProblem();
+        var actual = problem.ValidIPAddress("2001:0db8:85a3:0:0:8A2E:0370:7334");
+        const string expected = "IPv6";
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void Test3()
+    {
+        var problem = new ValidateIPAddressProblem();
+        var actual = problem.ValidIPAddress("256.256.256.256");
+        const string expected = "Neither";
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void Test4()
+    {
+        var problem = new ValidateIPAddressProblem();
+        var actual = problem.ValidIPAddress("01.01.01.01");
+        const string expected = "Neither";
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void Test5()
+    {
+        var problem = new ValidateIPAddressProblem();
+        var actual = problem.ValidIPAddress("192.0.0.1");
+        const string expected = "IPv4";
+
+        Assert.Equal(expected, actual);
+    }
+}
